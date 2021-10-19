52a481fbf97dbb56e1e803bf915b6069059b86a3
Matveibos <71291376+Matveibos@users.noreply.github.com>
2021-10-19 20:59:06 +0300

    Add files via upload

---
+++ LastStep/app/sin.rb
@@ -0,0 +1,40 @@
+require 'sinatra'
+
+class Dataes
+
+  def initialize(key, value)
+    @key = key
+    @value =value
+  end
+
+  def to_json
+    {key: @key, value: @value}.to_json
+  end
+
+end
+
+key1=Dataes.new(1,2).to_json
+key2=Dataes.new(2,3).to_json
+datas = []
+
+get '/' do
+  "hello matvey"
+end
+
+
+post'/data'do
+  content_type :json
+  body=JSON.parse request.body.read
+  datas.push(Dataes.new(body['key'],body['value']).to_json)
+  datas.to_json
+
+end
+
+get '/data' do
+  content_type :json
+  datas.to_json
+end
+
+
+
+
